2e7e7a9a1ca7138576d0909aebf41a728bc2bf2f
adamaho <aaho.public@gmail.com>
2020-02-01 14:34:30 -0500

    adding the start to the docs

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -10,6 +10,8 @@ export const config: Config = {
   namespace: 'nithview',
   bundles: [
     { components: ['nv-button'] },
+    { components: ['nv-panel'] },
+    { components: ['nv-card'] },
   ],
   outputTargets: [
     {
---
+++ docs/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="root">
+      <nv-panel>
+        <nv-button>asdfasdf</nv-button>
+      </nv-panel>
+    </div>
+  );
+}
+
+export default App;
