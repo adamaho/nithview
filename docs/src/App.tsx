2b29f4fd06e0ac6098e81d6aa675c198343fecd6
adamaho <aaho.public@gmail.com>
2020-02-01 15:09:47 -0500

    test push for netlify

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 const App = () => {
   return (
     <div className="root">
-      <nv-panel>
-        <nv-button>asdfasdf</nv-button>
+      <nv-panel clickable>
+        <nv-button
+          type="primary"
+          onClick={() => console.log('asdfasdf')}
+        >
+          asdfasdf
+        </nv-button>
       </nv-panel>
     </div>
   );
